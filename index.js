aa340d0bf8d72f4f82d54fd801763f768fc153d5
A-r-v-i <babuaravindsv@gmail.com>
2020-06-16 23:24:13 +0530

    password

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ module.exports = function sample(str) {
 
 module.exports = function dotter(str) {
   return str.replace(" ", ".");
-}
+};
+
+module.exports = function cryptit(str) {
+  const password = str;
+  const arr = password.split("");
+  let num;
+  arr.forEach(function (c) {
+    num = Number(c);
+    //case "integers"
+    if (!isNaN(num)) {
+      if (num % 2 == 0) {
+        res += String.fromCharCode(57 - num) + "&";
+      } else {
+        res += String.fromCharCode(num + 103) + "#";
+      }
+    } else {
+      res += c;
+    }
+  });
+  return res;
+};
